25628019564f2d864259f64152a8235b312dfc53
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-12-16 17:08:06 -0300

    Fin seccion 4

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,7 +1,11 @@
+import { CartCounter } from "@/app/shopping-cart";
+
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+
+      <CartCounter />
     </div>
   );
 }
