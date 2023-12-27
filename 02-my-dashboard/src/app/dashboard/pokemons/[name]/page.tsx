7f1a2ef4ede3e35a2aa91a9d30c801d2cd757edb
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2023-12-27 01:35:24 -0300

    fin seccion 6

--- 02-my-dashboard/src/app/dashboard/pokemons/[name]/page.tsx
+++ 02-my-dashboard/src/app/dashboard/pokemons/[name]/page.tsx
@@ -1,14 +1,14 @@
-import { Pokemon } from "@/pokemons";
+import { Pokemon, PokemonsResponse } from "@/pokemons";
 import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
 interface Props {
-  params: { id: string };
+  params: { name: string };
 }
 // SEO - Generacion de metadata y props estaticas del lado del servidor
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { id, name } = await getPokemon(params.id);
+  const { id, name } = await getPokemon(params.name);
   return {
     title: `# ${id} - ${name}`,
     description: `Pagina del pokemon ${name} `,
@@ -16,19 +16,23 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 }
 
 export async function generateStaticParams() {
-  const static151Prokemons = Array.from({ length: 151 }, (_, i) => `${i + 1}`).map(
-    (id) => ({
-      id: id,
-    })
-  );
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=151`
+  ).then((res) => res.json());
 
-  return static151Prokemons;
+  const static151Prokemons = data.results.map((pokemon) => ({
+    name: pokemon.name,
+  }));
+
+  return static151Prokemons.map(({ name }) => ({
+    name: name,
+  }));
 }
 
 // Obtenemos el pokemon
-const getPokemon = async (id: string): Promise<Pokemon> => {
+const getPokemon = async (name: string): Promise<Pokemon> => {
   try {
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
       next: { revalidate: 60 * 60 * 30 * 6 },
     }).then((res) => res.json());
 
@@ -40,7 +44,7 @@ const getPokemon = async (id: string): Promise<Pokemon> => {
 
 // page
 export default async function PokemonPage({ params }: Props) {
-  const pokemon = await getPokemon(params.id);
+  const pokemon = await getPokemon(params.name);
 
   return (
     <div className="flex mt-5 flex-col items-center text-slate-800">
