b8c1ebda248d399e705b1fcedb03f9694858c4df
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-01-06 07:47:51 -0300

    Add provider redux

---
+++ 02-my-dashboard/src/store/Providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "./index";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function Providers({ children }: Props) {
+  return <Provider store={store}>{children}</Provider>;
+}
+
+export default Providers;
