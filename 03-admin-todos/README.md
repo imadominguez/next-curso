e2e74bea8bacf4d841293c0e88007b835ebafd92
Imanol Dominguez Sanchez <imanol.desarrolloweb@gmail.com>
2024-01-10 09:24:55 -0300

    configuracion de prisma

--- 03-admin-todos/README.md
+++ 03-admin-todos/README.md
@@ -10,9 +10,12 @@ docker compose up -d
 
 2. Renombrar el .env.template a .env
 3. Reemplazar las variables de entorno
+4. Ejecutar el SEED para [rellenar la base de datos](localhost:3000/api/seed)
 
 # Prisma comandos
 
 ```
 npx prisma init
+npx prisma migrate dev
+npx prisma generate
 ```
---
+++ 03-admin-todos/src/app/api/seed/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/lib/primsa";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(request: Request) {
+  await prisma.todo.deleteMany(); // dele * from todo
+
+  await prisma.todo.createMany({
+    data: [
+      {
+        description: "description 1",
+      },
+      {
+        description: "description 2",
+        complete: true,
+      },
+      {
+        description: "description 3",
+      },
+    ],
+  });
+
+  // const todo = await prisma.todo.create({
+  //   data: { description: "Piedra del alma", complete: true },
+  // });
+  // console.log(todo);
+  return NextResponse.json({
+    message: "seed",
+  });
+}
