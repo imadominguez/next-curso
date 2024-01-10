8f239de2c055e8746e8142faf8ebfe4ee7d52c8b
Imanol Dominguez <imanol.desarrolloweb@gmail.com>
2024-01-10 14:36:37 -0300

    Fin seccion 9

--- 03-admin-todos/src/app/api/todos/[id]/route.ts
+++ 03-admin-todos/src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from "@/lib/primsa";
+import { Todo } from "@prisma/client";
 import { NextResponse } from "next/server";
+import { boolean, object, string } from "yup";
 
 interface Segments {
   params: {
@@ -7,28 +9,42 @@ interface Segments {
   };
 }
 
+const getTodo = async (id: string): Promise<Todo | null> => {
+  const todo = await prisma.todo.findFirst({ where: { id } });
+  return todo;
+};
+
 // * GET /todos/:id
 export async function GET(request: Request, { params }: Segments) {
   const { id } = params;
-  const todo = await prisma.todo.findFirst({ where: { id } });
-  if (!todo)
-    return NextResponse.json(
-      { message: `Todo con id ${id} no existe` },
-      { status: 404 }
-    );
+  const todo = await getTodo(id);
+  if (!todo) return NextResponse.json({ message: `Todo con id ${id} no existe` }, { status: 404 });
 
   return NextResponse.json(todo);
 }
 
+// ? Schema for PUT request body
+const puitSchema = object({
+  complete: boolean().optional(),
+  description: string().optional(),
+});
+
 // * PUT /todos/:id
 export async function PUT(request: Request, { params }: Segments) {
   const { id } = params;
-  const todo = await prisma.todo.findFirst({ where: { id } });
-  if (!todo)
-    return NextResponse.json(
-      { message: `Todo con id ${id} no existe` },
-      { status: 404 }
-    );
 
-  return NextResponse.json(todo);
+  const todo = await getTodo(id);
+  if (!todo) return NextResponse.json({ message: `Todo con id ${id} no existe` }, { status: 404 });
+  try {
+    const { complete, description } = await puitSchema.validate(await request.json());
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { complete, description },
+    });
+
+    return NextResponse.json(updatedTodo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  }
 }
